83fa567074d809884626db347fbbbd1dca7474da
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-15 10:47:59 +0200

    another way of creating db models

--- app/db.py
+++ app/db.py
@@ -1,17 +1,29 @@
 from sqlalchemy import MetaData, Column, Integer, String, Table
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 
-meta = MetaData()
+Base = declarative_base()
 
 
-link = Table(
-    'links', meta,
+class Link(Base):
 
-    Column('id', Integer, primary_key=True),
-    Column('link', String(150), nullable=False),
-    Column('data', String(200), nullable=False)
-)
+    __tablename__ = 'links'
+
+    id = Column(Integer, primary_key=True)
+    link = Column(String(150), nullable=False)
+    agregated_data = Column(String(200), nullable=False)
 
 
 async def pg_context(app):
-    conf = app['conf']['postgres']
+
+    conf = app['config']['postgres']
+    url_db = f"postgresql://{conf['user']}:{conf['password']}@{conf['host']}/{conf['database']}"
+    DBSession = sessionmaker(bind=create_engine(url_db))
+    session = DBSession()
+    app['db_session'] = session
+
+    yield
+
+    app['db_session'].close()
