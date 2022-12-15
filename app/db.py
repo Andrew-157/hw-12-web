8f007cf936c0a2556db0798a867bcf1b5564af53
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-15 10:17:54 +0200

    models for database

---
+++ app/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import MetaData, Column, Integer, String, Table
+
+
+meta = MetaData()
+
+
+link = Table(
+    'links', meta,
+
+    Column('id', Integer, primary_key=True),
+    Column('link', String(150), nullable=False),
+    Column('data', String(200), nullable=False)
+)
